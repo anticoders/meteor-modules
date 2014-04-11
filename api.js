eb6f3276cf78b0e8452be4fc3c88b09db1ffce3c
apendua <apendua@gmail.com>
2014-04-11 16:56:05 +0200

    Dreaming about some nice API

--- api.js
+++ api.js
@@ -1,14 +1,50 @@
 
+// how the API could work
+
 module = function (name) {
-  
+
 }
+
+// these guys need to be loaded before everything
+
+module('blog').depend([
+  'triggers',
+  'actions',
+  'router',
+]);
+
+// this part of code will be loaded instantly, on both server and client
+
+module('blog').define(function (Blog, settings, i18n) {
+
+});
+
+// this code will be loaded on demand
+
+module('blog').define('editor', [], function () {
+  // only admin will require this one
+
+});
+
+module('blog').widget('recent', [], function (Blog, settings, i18n) {
+
+  // template goes here (?)
+  Blog.widget.recent
+
+});
+
+// this will be loaded as soon as the blog module is required
 
 module('blog').extend(function (Blog, settings, i18n) {
 
-  Blog.widget('recentPost', function () {
-
-
-
+  Blog.router(function () {
+    this.route({
+      path: '/edit',
+      waitOn: function () {
+        return Blog.require('editor');
+      },
+    });
   });
 
 });
+

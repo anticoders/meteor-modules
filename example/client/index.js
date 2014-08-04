d9f952843a94edd210b7b1d5c9694ac4ff48ce33
apendua <apendua@gmail.com>
2014-08-04 10:14:54 +0200

    Added iron-router to the example app

--- example/client/index.js
+++ example/client/index.js
@@ -1,3 +1,26 @@
+
+Router.onBeforeAction('loading');
+Router.configure({
+  loadingTemplate: 'loading'
+});
+
+Router.map(function () {
+
+  this.route('home', {
+    path: '/',
+  });
+
+  this.route('myModule_dashboard', {
+    path: '/dashboard',
+    waitOn: function () {
+      return MyModule.require(['dashboard']);
+    },
+  })
+
+});
+
+
+
 UI.registerHelper('myModuleHome', function () {
   return MyModule.Template.home;
 });

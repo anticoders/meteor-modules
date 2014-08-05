64211c18879c7a7a27b5c4e9a9d787c155d297a7
apendua <apendua@gmail.com>
2014-08-05 11:51:42 +0200

    Handle template helpers in a more clever way

--- plugins.js
+++ plugins.js
@@ -12,6 +12,68 @@ Module.registerPlugin('$template', [ '$module' ], function ($module) {
     }
 
     var _Template = { prototype: constructor.prototype };
+    var _hasOwnProperty = Object.prototype.hasOwnProperty;
+    var _globalHelpers = {};
+
+    var bindDataContext = function (x) {
+      if (typeof x === 'function') {
+        return function () {
+          var data = Blaze.getCurrentData();
+          if (data == null)
+            data = {};
+          return x.apply(data, arguments);
+        };
+      }
+      return x;
+    };
+
+    var wrapHelper = function (f) {
+      return Blaze.wrapCatchingExceptions(f, 'template helper');
+    };
+
+    _Template.__lookup__ = function (templateName) {
+      if (!_hasOwnProperty.call(_Template, templateName))
+        return null;
+      var tmpl = _Template[templateName];
+      if (Template.__isTemplate__(tmpl))
+        return tmpl;
+      return null;
+    };
+
+    _Template.__create__ = function (viewName, templateFunc, initView) {
+      var tmpl = new _Template.prototype.constructor;
+      tmpl.__viewName = viewName;
+      tmpl.__render = templateFunc;
+      if (initView)
+        tmpl.__initView = initView;
+      return tmpl;
+    };
+
+    _Template.prototype.__makeView = function () {
+      var view = Template.prototype.__makeView.apply(this, arguments);
+      
+      view.lookup = function (name, _options) {
+
+        var template = this.template;
+        var lookupTemplate = _options && _options.template;
+
+        if (/^\./.test(name)) {
+          if (!/^(\.)+$/.test(name))
+            throw new Error("id starting with dot must be a series of dots");
+          return Blaze._parentData(name.length - 1, true /*_functionWrapped*/);
+        } else if (template && (name in template)) {
+          return wrapHelper(bindDataContext(template[name]));
+        } else if (lookupTemplate && _Template.__lookup__(name)) {
+          return _Template.__lookup__(name);
+        } else if (_globalHelpers[name]) {
+          return wrapHelper(bindDataContext(_globalHelpers[name]));
+        }
+
+        return Blaze.View.prototype.lookup.apply(this, arguments)
+      }
+
+      return view;
+    }
 
     if (typeof Template !== 'undefined') {
       _Template.__create__ = _.bind(Template.__create__, Template);
@@ -37,7 +99,7 @@ Module.registerPlugin('$template', [ '$module' ], function ($module) {
 
       _Template[templateName] = tmpl;
 
-      _Template.prototype[templateName] = tmpl;
+      //_Template.prototype[templateName] = tmpl;
 
       if (typeof Template !== 'undefined') {
         Template[templateFullName] = tmpl;
@@ -51,10 +113,10 @@ Module.registerPlugin('$template', [ '$module' ], function ($module) {
     // "global" helpers scoped to the module namespace
 
     instance.registerHelper = function (helperName, helperFunc) {
-      if (_Template.prototype.hasOwnProperty(helperName)) {
+      if (_globalHelpers.hasOwnProperty(helperName)) {
         throw new Error("Helper " + helperName + " already exists.");
       }
-      _Template.prototype[helperName] = helperFunc;
+      _globalHelpers[helperName] = helperFunc;
     };
 
     instance.getTemplateFullName = function (templateName) {
